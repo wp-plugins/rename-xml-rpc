2ca5e4fd2059a1b1c776160891a2e862f9795f29
daniloercoli <daniloercoli@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-21 10:20:26 +0000

    fixed the RSD document link
    git-svn-id: https://plugins.svn.wordpress.org/rename-xml-rpc/trunk@441400 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === Plugin Name ===
-Contributors: koke, automattic
+Contributors: koke, daniloercoli, automattic
 Tags: xmlrpc
 Requires at least: 2.9
 Tested up to: 3.2.1
-Stable tag: 1.0
+Stable tag: 1.1
 
 Make XML-RPC work if you rename the file. Some hosts block access to xmlrpc.php file making it impossible to use
 
@@ -12,5 +12,12 @@ Make XML-RPC work if you rename the file. Some hosts block access to xmlrpc.php 
 This section describes how to install the plugin and get it working.
 
 1. Upload `rename-xml-rpc.php` to the `/wp-content/plugins/` directory
-1. Activate the plugin through the 'Plugins' menu in WordPress
-1. There is no step 3
+2. Upgrade the filename of the XML-RPC Endpoint in `rename-xml-rpc.php` (sorry there is not an option page yet!)
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. There is no step 4
+
+
+== Changelog ==
+
+= 1.1 =
+Fix the RSD document link by using the renamed filepath 
--- rename-xml-rpc.php
+++ rename-xml-rpc.php
@@ -4,12 +4,20 @@ Plugin Name: Rename XMLRPC
 Plugin URI: http://wordpress.org/extend/plugins/rename-xml-rpc/
 Description: Make XML-RPC work if you rename the file. Some hosts block access to xmlrpc.php file making it impossible to use
 Author: Jorge Bernal
-Version: 1.0
+Version: 1.1
 Author URI: http://koke.me
 */
 
+
+remove_action('wp_head','rsd_link');
+function renamed_rsd_link() {
+	$renamed_xml_rpc_filename = 'xmlrpc2.php'; //CHANGE THIS poiting to the renamed file
+	echo '<link rel="EditURI" type="application/rsd+xml" title="RSD" href="' . get_bloginfo('wpurl') . "/$renamed_xml_rpc_filename?rsd\" />\n";
+}
+add_action( 'wp_head', 'renamed_rsd_link' );
+
 function rx_site_url( $url, $path, $orig_scheme, $blog_id ) {
-  if ( XMLRPC_REQUEST  && $path == 'xmlrpc.php' ) {
+  if ( defined( 'XMLRPC_REQUEST' )  && $path == 'xmlrpc.php' ) {
     return preg_replace( '/xmlrpc.php$/', basename( $_SERVER['PHP_SELF'] ), $url );
   } else {
     return $url;
